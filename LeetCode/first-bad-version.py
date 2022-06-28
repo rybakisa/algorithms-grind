465310489963ef78ad2d75c21b6b70408bf16f59
Andrey Ezhov <rybakisa@gmail.com>
2022-06-28 11:36:54 +0400

    Improve  first bad version solution
    Add solution with one isBadVersion call per iteration.
    In initial solution we checking the cell's neighbors every time which is suboptimal.
    More efficient is to narrow the range to 2 numbers asap.

--- LeetCode/first-bad-version.py
+++ LeetCode/first-bad-version.py
@@ -1,41 +1,56 @@
-FIRST_BAD = 44444
-NUMBER = 132532
+FIRST_BAD = 14
 
 def isBadVersion(n: int) -> bool:
     return n >= FIRST_BAD
 
 
-def firstBadVersion(n: int) -> int:
-    min = 0
-    max = n
-    current = n // 2
-    
+
+def firstBadVersion_2checks(n: int) -> int:
+    min, max = 0, n
+
     while min <= max:
-        print(min, max, current)
+        current = (max + min) // 2
+
         if isBadVersion(current):
             # check previous
             if isBadVersion(current - 1):
                 # two bad versions
                 # need to check prev versions
                 max = current - 2
-                print('bb')
             else:
-                print('gb')
                 return current
                 
         else:
             # check next
             if isBadVersion(current + 1):
-                print('gb')
                 return current + 1
             else:
                 # two good versions
                 # need to check further versions
-                print('gg')
                 min = current + 2
-        current = (max + min) // 2
 
 
-print(
-    'Answer: ', firstBadVersion(NUMBER)
-)
+def firstBadVersion(n: int) -> int:
+    min, max = 1, n
+
+    while min < max:
+        current = (min + max) // 2
+
+        if isBadVersion(current):
+            max = current
+        else:
+            min = current + 1
+    
+    return max
+
+
+
+cases = [
+    123,
+    132532,
+    444444444444,
+]
+for num in cases:
+    print('Answer: ', firstBadVersion(num))
+    print('Answer: ', firstBadVersion_2checks(num))
+    print()
